5735755e9340b1aafcc4542c8f07521232513657
Nashir Janmohamed <nashirbass@gmail.com>
2020-03-19 10:35:37 -0700

    Created CLion project for backend

---
+++ cmake-build-debug/CMakeFiles/ElectrifEye.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/Charge.cpp" "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/cmake-build-debug/CMakeFiles/ElectrifEye.dir/Charge.cpp.o"
+  "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/Point.cpp" "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/cmake-build-debug/CMakeFiles/ElectrifEye.dir/Point.cpp.o"
+  "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/World.cpp" "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/cmake-build-debug/CMakeFiles/ElectrifEye.dir/World.cpp.o"
+  "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/main.cpp" "/Users/nashish/Documents/Education/CS/projects/ElectrifEye/cmake-build-debug/CMakeFiles/ElectrifEye.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ElectrifEye.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ElectrifEye.dir/Charge.cpp.o"
+  "CMakeFiles/ElectrifEye.dir/Point.cpp.o"
+  "CMakeFiles/ElectrifEye.dir/World.cpp.o"
+  "CMakeFiles/ElectrifEye.dir/main.cpp.o"
+  "ElectrifEye"
+  "ElectrifEye.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ElectrifEye.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
