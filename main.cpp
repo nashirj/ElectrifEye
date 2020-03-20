92a526ec6783012ffcdefbff9e024743a546b01c
Nashir Janmohamed <nashirbass@gmail.com>
2020-03-20 00:00:39 -0700

    Wrote real driver code to test objects and controllers

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include "Charge.h"
+#include "Point.h"
+#include "PointCharge.h"
+#include "ElectricFieldController.h"
+#include "GeometryController.h"
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    std::cout << "Testing that valgrind detects memory leaks" << std::endl;
-    int* blah = new int[10];
-    std::unique_ptr<Charge> charge1 = std::make_unique<Charge>(1);
-    std::unique_ptr<Charge> charge2 = std::make_unique<Charge>(-1);
+//    std::cout << "Testing that valgrind detects memory leaks" << std::endl;
+//    int* blah = new int[10];
+
+    std::shared_ptr<Charge> charge1 = std::make_shared<Charge>(.001);
+    std::shared_ptr<Charge> charge2 = std::make_shared<Charge>(-.001);
+
+    std:: cout << charge1->getMagnitude() << std::endl;
+    std:: cout << charge2->getMagnitude() << std::endl;
+
+    std::shared_ptr<Point> point1 = std::make_shared<Point>(1, 0);
+    std::shared_ptr<Point> point2 = std::make_shared<Point>(-1, 0);
+
+    std:: cout << point1->getCoordinates()->first << std::endl;
+    std:: cout << point1->getCoordinates()->second << std::endl;
+    std:: cout << "hi" << std::endl;
+    std:: cout << point2->getCoordinates()->first << std::endl;
+    std:: cout << point2->getCoordinates()->second << std::endl;
+
+    std::shared_ptr<PointCharge> point_charge1 = std::make_shared<PointCharge>(charge1, point1);
+    std::shared_ptr<PointCharge> point_charge2 = std::make_shared<PointCharge>(charge1, point2);
+
+    std::cout << ElectricFieldController::getForceMagnitude(point_charge1, point_charge2) << std::endl;
+
+    std::cout << GeometryController::getEuclideanDistance(point_charge1->getLocation(), point_charge2->getLocation());
 
     return 0;
 }
