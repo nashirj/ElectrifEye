0e12c7e681db3c59f6fcb331e648343adfc485ed
Nashir Janmohamed <nashirbass@gmail.com>
2020-03-24 23:15:15 -0700

    Added pi to constants

--- include/ElectrifEye/Constants.h
+++ include/ElectrifEye/Constants.h
@@ -8,6 +8,9 @@
 namespace constants {
     // Coulomb constant, units of (N*m^2)/(C^2)
     static constexpr float k = 8.987551792 * 1000000000;
+
+    // Pi mathematical constant, unitless
+    static constexpr float pi = 3.14159;
 };
 
 #endif //ELECTRIFEYE_CONSTANTS_H
