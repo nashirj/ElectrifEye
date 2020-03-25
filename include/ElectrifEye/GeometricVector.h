01b855e05c9aababf856fc3d517735d8b387d6b8
Nashir Janmohamed <nashirbass@gmail.com>
2020-03-24 23:14:51 -0700

    imported GeometricVector class from Michael Estes

--- include/ElectrifEye/GeometricVector.h
+++ include/ElectrifEye/GeometricVector.h
@@ -5,9 +5,37 @@
 #ifndef ELECTRIFEYE_GEOMETRICVECTOR_H
 #define ELECTRIFEYE_GEOMETRICVECTOR_H
 
+#include <cmath>
+#include "Constants.h"
+
+/**
+ * Thanks Michael Estes for sharing a gist for this code: https://gist.github.com/MichaelEstes/35c26ccb04f657d753a3
+ */
 
 class GeometricVector {
+    float x, y, z;
+    GeometricVector() : x(0), y(0), z(0){};
+    GeometricVector(float X, float Y, float Z) : x(X), y(Y), z(Z) {}
 
+    float getLength() const { return sqrt((x * x) + (y * y) + (z * z)); }
+    void normalize(){ *this /= getLength(); }
+    GeometricVector normalized(){ return *this /= getLength(); }
+    float dotProduct(GeometricVector rightVec) const { return (x * rightVec.x) + (y * rightVec.y) + (z * rightVec.z); }
+    GeometricVector crossProduct(GeometricVector);
+    float angle(GeometricVector);
+    float greaterAngle(GeometricVector rightVec){ return float((2 * constants::pi) - angle(rightVec)); }
+
+    GeometricVector operator+ (GeometricVector) const;
+    GeometricVector operator- (GeometricVector) const;
+    GeometricVector operator* (float) const;
+    GeometricVector operator/ (float) const;
+    GeometricVector& operator+= (GeometricVector);
+    GeometricVector& operator-= (GeometricVector);
+    GeometricVector& operator*= (float);
+    GeometricVector& operator/= (float);
+    GeometricVector& operator= (GeometricVector);
+    bool operator== (const GeometricVector&) const;
+    bool operator!= (const GeometricVector&) const;
 };
 
 
--- src/GeometricVector.cpp
+++ src/GeometricVector.cpp
@@ -2,4 +2,77 @@
 // Created by Nashir Janmohamed on 3/24/20.
 //
 
-#include "GeometricVector.h"
+#include "ElectrifEye/GeometricVector.h"
+
+GeometricVector GeometricVector::crossProduct(GeometricVector rightVec){
+    return GeometricVector((y * rightVec.z) - (z * rightVec.y),
+                        (z * rightVec.x) - (x * rightVec.z),
+                        (x * rightVec.y) - (y * rightVec.x));
+}
+
+float GeometricVector::angle(GeometricVector rightVec){
+    GeometricVector tempVec = normalized();
+    float temp = tempVec.dotProduct(rightVec.normalized());
+    return float(acos(temp));
+}
+
+GeometricVector GeometricVector::operator+(GeometricVector rightVec)const{
+    return GeometricVector(*this) += rightVec;
+}
+
+GeometricVector GeometricVector::operator-(GeometricVector rightVec) const{
+    return GeometricVector(*this) -= rightVec;
+}
+
+GeometricVector GeometricVector::operator*(float scalar) const{
+    return GeometricVector(*this) *= scalar;
+}
+
+GeometricVector GeometricVector::operator/(float scalar) const{
+    return GeometricVector(*this) /= scalar;
+}
+
+GeometricVector& GeometricVector::operator+=(GeometricVector rightVec){
+    x += rightVec.x;
+    y += rightVec.y;
+    z += rightVec.z;
+    return *this;
+}
+
+GeometricVector& GeometricVector::operator-=(GeometricVector rightVec){
+    x -= rightVec.x;
+    y -= rightVec.y;
+    z -= rightVec.z;
+    return *this;
+}
+
+GeometricVector& GeometricVector::operator*=(float scalar){
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    return *this;
+}
+
+GeometricVector& GeometricVector::operator/=(float scalar){
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    return *this;
+}
+
+GeometricVector& GeometricVector::operator=(GeometricVector rightVec){
+    x = rightVec.x;
+    y = rightVec.y;
+    z = rightVec.z;
+    return *this;
+}
+
+bool GeometricVector::operator==(const GeometricVector& rightVec) const{
+    return (x == rightVec.x &&
+            y == rightVec.y &&
+            z == rightVec.z);
+}
+
+bool GeometricVector::operator!=(const GeometricVector& rightVec) const{
+    return !operator==(rightVec);
+}
